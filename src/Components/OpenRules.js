bded6d1c7f45d440aef302b30288615a57dd07ba
nginnever <ginneversource@gmail.com>
2019-10-24 14:21:07 -0700

    update

--- src/Components/OpenRules.js
+++ src/Components/OpenRules.js
@@ -11,7 +11,7 @@ class OpenRules extends React.Component {
       <div style={{display: 'flex', justifyContent: 'center', textAlign:'left'}}>
       <p style={{width:'800px'}}>
         The WCL-Open rules are currently being negotiated. We would like to hear from the community as well as experienced players while generating these rules.
-        Feel free to stop by the WCL discord to join the conversation. We will update the rules to provide as fair and interesting playing field as possible at anytime.
+        Feel free to stop by the WCL discord to join the conversation. We will update the rules to provide as fair and interesting duels as possible at anytime.
       </p>
       </div>
       <br/>
