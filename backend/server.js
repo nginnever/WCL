80dc2b5b4bbddd026f72706b9a8f1adac9e4a357
nginnever <ginneversource@gmail.com>
2019-10-28 17:38:45 -0700

    pathing fix

--- backend/server.js
+++ backend/server.js
@@ -114,6 +114,11 @@ app.use('/api', router);
 
 app.use("/", express.static(__dirname + "/../build"))
 
+//app.use(express.static(path.join(__dirname, '/../build')));
+app.get('/*', function(req, res) {
+    res.sendfile(path.resolve('../build/index.html'));
+});
+
 // app.use(express.static(path.join(__dirname, '/../build')));
 // app.get('*', function (req, res) {
 //   res.sendFile(path.join(__dirname, 'build', 'index.html'));
