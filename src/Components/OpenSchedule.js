ce753cdc98ed55e3bcb16a4d33e321f48d7c8dda
nginnever <ginneversource@gmail.com>
2019-11-05 22:17:04 -0800

    favicon

--- src/Components/OpenSchedule.js
+++ src/Components/OpenSchedule.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 class OpenSchedule extends React.Component {
   render() {
     return <div>
-      <h4> WCL-Open Schedule Coming Soon</h4>
+      <h4> WCL Open schedule coming soon.</h4>
       <br/>
       <br/>
     </div>
